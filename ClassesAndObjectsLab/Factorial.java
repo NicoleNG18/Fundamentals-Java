d5291264085e9796d4b335ac6dda923545c9e280
Nikol Georgieva <nicole.georgieva18@abv.bg>
2022-05-19 21:13:03 +0300

    Add files via upload

---
+++ ClassesAndObjectsLab/Factorial.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=Integer.parseInt(scanner.nextLine());
+        BigInteger factoriel=new BigInteger(String.valueOf(1));
+        for(int i=1;i<=n;i++){
+            factoriel=factoriel.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(factoriel);
+    }
+}
